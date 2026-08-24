4dace241681fd0e67a42a521fa724310e2c41719
dannhpaul2006-pixel <dannhpaul2006@gmail.com>
2026-08-24 10:09:13 -0500

    Update matriz5x5 .py

--- matriz-5x5/matriz5x5 .py
+++ matriz-5x5/matriz5x5 .py
@@ -1,12 +1,10 @@
 matriz = [[0 for _ in range(5)] for _ in range(5)]
 
-# Ingresar valores con bucles anidados
 for i in range(5):
     for j in range(5):
         valor = int(input(f"Ingrese el valor para la posición [{i+1}][{j+1}]: "))
         matriz[i][j] = valor
 
-# Mostrar la matriz organizada
 print("\nMatriz ingresada:")
 for i in range(5):
     for j in range(5):
