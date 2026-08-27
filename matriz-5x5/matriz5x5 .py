28765126f4d7de7b420cb7b4494949c61736beb9
danny-11b <bravobravodanny901@gmail.com>
2026-08-27 14:03:41 -0500

    Update matriz5x5 .py

--- matriz-5x5/matriz5x5 .py
+++ matriz-5x5/matriz5x5 .py
@@ -1,12 +1,12 @@
-matriz = [[0 for _ in range(5)] for _ in range(5)]
+matriz = [[0 for _ in range(4)] for _ in range(4)]
 
 for i in range(5):
     for j in range(5):
-        valor = int(input(f"Ingrese el valor para la posición [{i+1}][{j+1}]: "))
+        valor = int(input(f"Ingrese los 25 valores para la matriz [{i+1}][{j+1}]: "))
         matriz[i][j] = valor
 
 print("\nMatriz ingresada:")
-for i in range(5):
-    for j in range(5):
+for i in range(4):
+    for j in range(4):
         print(matriz[i][j], end="\t")
     print()
